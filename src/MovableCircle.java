1be3ee620fed83332ed81105fb72a6b3c4c240df
Ikrom27 <ikrom.hasanbaev.27@gmail.com>
2023-09-30 10:22:39 +0300

    Initial commit

---
+++ src/MovableCircle.java
@@ -0,0 +1,24 @@
+public class MovableCircle implements Movable{
+    private int radius;
+    private MovablePoint center;
+
+    @Override
+    public void moveUp() {
+
+    }
+
+    @Override
+    public void moveDown() {
+
+    }
+
+    @Override
+    public void moveLeft() {
+
+    }
+
+    @Override
+    public void moveRight() {
+
+    }
+}
---
+++ src/MovableRectangle.java
@@ -0,0 +1,23 @@
+public class MovableRectangle implements Movable{
+    private MovablePoint topleft;
+    private MovablePoint bottomright;
+    @Override
+    public void moveUp() {
+
+    }
+
+    @Override
+    public void moveDown() {
+
+    }
+
+    @Override
+    public void moveLeft() {
+
+    }
+
+    @Override
+    public void moveRight() {
+
+    }
+}
